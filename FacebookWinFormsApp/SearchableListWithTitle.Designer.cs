e889d51993c04c183561b8950e93abd396ab90e6
Yaniv Zamir <yanivza@mta.ac.il>
2024-12-09 13:03:59 +0200

    Delete unnessery classes

--- FacebookWinFormsApp/SearchableListWithTitle.Designer.cs
+++ FacebookWinFormsApp/SearchableListWithTitle.Designer.cs
@@ -41,8 +41,9 @@
             this.labelTitle.Font = new System.Drawing.Font("Mistral", 18F, System.Drawing.FontStyle.Italic);
             this.labelTitle.ForeColor = System.Drawing.Color.RosyBrown;
             this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(49, 29);
+            this.labelTitle.Size = new System.Drawing.Size(58, 35);
             this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "label";
             // 
@@ -51,9 +52,11 @@
             this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox.ForeColor = System.Drawing.Color.Brown;
             this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(0, 49);
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(0, 55);
+            this.listBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(421, 304);
+            this.listBox.Size = new System.Drawing.Size(561, 379);
             this.listBox.TabIndex = 2;
             this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
             // 
@@ -61,21 +64,23 @@
             // 
             this.textBoxSearch.Dock = System.Windows.Forms.DockStyle.Top;
             this.textBoxSearch.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.textBoxSearch.Location = new System.Drawing.Point(0, 29);
+            this.textBoxSearch.Location = new System.Drawing.Point(0, 35);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(421, 20);
+            this.textBoxSearch.Size = new System.Drawing.Size(561, 20);
             this.textBoxSearch.TabIndex = 1;
             this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
             // 
             // SearchableListWithTitle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.textBoxSearch);
             this.Controls.Add(this.labelTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SearchableListWithTitle";
-            this.Size = new System.Drawing.Size(421, 353);
+            this.Size = new System.Drawing.Size(561, 434);
             this.ResumeLayout(false);
             this.PerformLayout();
 
