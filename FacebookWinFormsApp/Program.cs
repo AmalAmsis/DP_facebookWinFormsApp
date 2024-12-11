45bce9dae0774935bc6bd441fa89298a76c9ebbf
Yaniv Zamir <yanivza@mta.ac.il>
2024-12-11 13:19:49 +0200

    Added Submission Document Link

--- FacebookWinFormsApp/Program.cs
+++ FacebookWinFormsApp/Program.cs
@@ -11,6 +11,10 @@ namespace BasicFacebookFeatures
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// <remarks>
+        /// Submission Document Link:
+        /// https://docs.google.com/document/d/1br6p_AzEXO-xRv8oDST8yZp-QIbCjZ4_0fmskwSslOg/edit?usp=sharing
+        /// </remarks>
         [STAThread]
         static void Main()
         {
